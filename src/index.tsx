bbc96dfb11da1e453f2ed4584f74121ea9b1f48c
Yoghantara <gyoghantara@gmail.com>
2023-01-25 08:37:52 +0800

    vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+import { inject } from "@vercel/analytics";
 import App from "./App";
 import "./index.css";
+
+inject();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
